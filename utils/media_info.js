8df92d1104a9e8ec91c4aec39cf6a22f12572071
james58899 <james59988@gmail.com>
2017-02-27 11:48:23 +0000

    ESLint

--- utils/media_info.js
+++ utils/media_info.js
@@ -1,19 +1,19 @@
 const Promise = require('bluebird');
 const execFile = require('child_process').execFile;
-const fixPathname = require("../utils/fix_pathname");
+const fixPathname = require('../utils/fix_pathname');
 
 function parse(url) {
 	url = fixPathname(url);
 	return new Promise(function (resolve, reject) {
-		execFile("ffprobe", ["-v", "error", "-show_entries", "stream_tags=title,artist:format_tags=title,artist", url], {timeout: 10000}, function(error, stdout, stderr) {
+		execFile('ffprobe', ['-v', 'error', '-show_entries', 'stream_tags=title,artist:format_tags=title,artist', url], {timeout: 10000}, function(error, stdout, stderr) {
 			if (error) {
 				if (error.code === 1) {
-					console.log("[MediaInfo] Error: Can't parse file " + url);
+					console.log('[MediaInfo] Error: Can\'t parse file ' + url);
 					reject(new Error(stderr));
 					return;
-				}else if (error.code === 130) {
-					console.log("[MediaInfo] Error: Fetch timeout " + url);
-					reject(new Error("Fetch timeout"));
+				} else if (error.code === 130) {
+					console.log('[MediaInfo] Error: Fetch timeout ' + url);
+					reject(new Error('Fetch timeout'));
 					return;
 				}
 				resolve({});
@@ -21,7 +21,7 @@ function parse(url) {
 			}
 
 			var title,artist;
-			if (stdout.match(/TAG:title=(.*)/i) && stdout.match(/TAG:artist=(.*)/i)){
+			if (stdout.match(/TAG:title=(.*)/i) && stdout.match(/TAG:artist=(.*)/i)) {
 				title = stdout.match(/TAG:title=(.*)/i)[1];
 				artist = stdout.match(/TAG:artist=(.*)/i)[1];
 			}
