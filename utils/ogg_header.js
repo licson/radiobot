115a6b1986a8b26d0b5a574dcaa19cc72ce774f7
mmis1000 <mmis10002@gmail.com>
2017-09-03 15:07:15 +0000

    Add util to rewrite opus Pre-skip value recommented by draft-ietf-codec-oggopus-14 and add option to disable http ogg endpoint

--- utils/ogg_header.js
+++ utils/ogg_header.js
@@ -1,3 +1,5 @@
+const crc32 = require('./crc32');
+
 // based on http://ietf.org/rfc/rfc3533.txt
 function indexOfHeader(buffer, from) {
     from = from || 0;
@@ -13,4 +15,25 @@ function indexOfHeader(buffer, from) {
     return -1;
 }
 
+// based http://ietf.org/rfc/rfc3533.txt
+// based on https://tools.ietf.org/html/draft-ietf-codec-oggopus-14#section-5
+function rewriteOpusPreSkip(buffer, newPreSkip) {
+    const CRCPosition = 22;
+    const preSkipPosition = 38;
+    const newBuffer = Buffer.alloc(buffer.length);
+    
+    newBuffer.fill(buffer);
+    newBuffer.writeUInt32LE(0, CRCPosition);
+    newBuffer.writeInt16LE(newPreSkip || 3840, preSkipPosition);
+    
+    const newCRC = crc32(newBuffer);
+    
+    console.log(newCRC);
+    
+    newBuffer.writeInt32LE(newCRC, CRCPosition);
+    
+    return newBuffer; 
+}
+
 exports.indexOfHeader = indexOfHeader;
+exports.rewriteOpusPreSkip = rewriteOpusPreSkip;
