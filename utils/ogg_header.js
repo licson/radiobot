a48d7319bfe7ba64424bb60de38e66f2d6e46729
mmis1000 <mmis10002@gmail.com>
2017-09-03 06:21:00 +0000

    Fix linter error

--- utils/ogg_header.js
+++ utils/ogg_header.js
@@ -2,18 +2,11 @@
 function indexOfHeader(buffer, from) {
     from = from || 0;
     
-    const char_O = 0x4f;
-    const char_g = 0x67;
-    const char_S = 0x53;
-    
-    // console.log(buffer.toString('utf8'))
-    
-    for (let i = from; i < buffer.length - 3; i++) {
-        
-        if (buffer[i] !== char_O) continue;
-        if (buffer[i + 1] !== char_g) continue;
-        if (buffer[i + 2] !== char_g) continue;
-        if (buffer[i + 3] !== char_S) continue;
+    for (var i = from; i < buffer.length - 3; i++) {
+        if (buffer[i] !== 0x4f) continue; // O
+        if (buffer[i + 1] !== 0x67) continue; // g
+        if (buffer[i + 2] !== 0x67) continue; // g
+        if (buffer[i + 3] !== 0x53) continue; // S
         return i;
     }
     
