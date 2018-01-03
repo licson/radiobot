687690a940df5fec643329a559956d18fac93278
鄭仁翔 <james58899@users.noreply.github.com>
2018-01-03 15:54:06 +0800

    Prefer opus & Fix sort

--- utils/youtube_info.js
+++ utils/youtube_info.js
@@ -16,12 +16,20 @@ function parse(url) {
 				return i.bitrate == null && i.audioBitrate;
 			});
 			
+			let opusFilter = res.formats.filter(function (i) {
+				return i.audioEncoding === 'opus';
+			});
+
+			if (opusFilter.length !== 0) {
+				selected = opusFilter
+			}
+
 			if (selected.length === 0) {
 				return reject(new Error('This video does not have any audio only format.'));
 			}
 			
 			selected = selected.sort(function (a, b) {
-				return a.audioBitrate >= b.audioBitrate ? 1 : -1;
+				return b.audioBitrate - a.audioBitrate;
 			})[0];
 			
 			resolve({
